baa8e49ba317850e969f01ec53ad1d80502e19e7
amirreza017 <amirreza.forootan@outlook.com>
2023-08-06 00:29:42 +0330

    bug fixed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,40 +1,48 @@
-import React, { useState } from 'react'
+import React, {  useEffect, useState } from 'react'
 import './Home.css'
 import EmojiList from '../../Data/EmojiList.json'
 
 
 const Home = () => {
-    
+ 
     // useState 
-    const [textValue,setTextValue] = useState('ffffffffffffffff')
+    const [textValue,setTextValue] = useState('')
+    const [emojiData,setEmojiData] = useState([]);
 
     // Functions
     const textOnChange = (event) => {
         setTextValue(event.target.value)
     }
-    
-    EmojiList.forEach((data,index) => {
-        if (EmojiList[index].title.includes(textValue) ) {
-            document.getElementById('container_result').innerHTML += `
-                <p className='para'>عنوان: <span id='result-title'>${EmojiList[index].title}</span></p>
-                <p className='para'>نماد: <span id='result-symbol'>${EmojiList[index].symbol}</span></p>
-                <p className='para'>کلمه کلیدی: <span id='result-keyword'>${EmojiList[index].keywords}</span></p>
-                <div className='border'></div>
-            `
+
+    useEffect(() => {
+        if (textValue === '') {
+            const result = EmojiList.filter(emj => emj.title.includes('محل'));
+            setEmojiData(result);
+        } else {
+            const result = EmojiList.filter(emj => emj.title.includes(textValue) || emj.keywords.includes(textValue));
+            setEmojiData(result);
         }
-    })
+    },[textValue])
+
+    
 
     // Return
     return (
-        <div className='container' id='container'>
+        <div className='container'>
             <h1 className='container_header'>جست و جوی Emoji</h1>
             <h3 className='container_header2'>عنوان یا کلمه کلیدی که Emoji مورد نظر را توصیف می کند را در فیلد زیر بنویسید</h3>
-            <input type='text' id='textvalueid' onChange={event => textOnChange(event)} />
-            <div className='container_result' id='container_result'>
-                <p className='para'>عنوان: <span id='result-title'></span></p>
-                <p className='para'>نماد: <span id='result-symbol'></span></p>
-                <p className='para'>کلمه کلیدی: <span id='result-keyword'></span></p>
-                <div className='border'></div>
+            <input type='text' id='textvalueid' onChange={textOnChange} />
+            <div className='container_result'>
+                {emojiData.map((data,index) => {
+                return (
+                    <div key={index}>
+                    <p><span className='para'>عنوان:</span> {data.title}</p>
+                    <p><span className='para'>نماد:</span> {data.symbol}</p>
+                    <p><span className='para'>کلمه کلیدی:</span> {data.keywords}</p>
+                    <div className='border'></div>
+                    </div>
+                    )
+                })}   
             </div>
         </div>
     )
